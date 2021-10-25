5cd8195fd30ea386e14f40ac377ec6793c0788cb
gmueller-ewerk <g.mueller@ewerk.com>
2021-10-25 13:48:57 +0000

    Deploying to gh-pages from @ digitalwarenkombinat/julala@781961ff0abe1825b19f57629cbedb81504ee308 🚀

--- mediaData.js
+++ mediaData.js
@@ -2,94 +2,102 @@ export const mediaData = {
   "isMaaS": true,
   "elementSuffix": ".webp",
   "colors": {
-    "primaryColor": "#b97439",
-    "secondaryColor": "#faebd7",
-    "backgroundColor": "#d7ccaa",
+    "primaryColor": "#477999",
+    "secondaryColor": "#add2d3",
+    "backgroundColor": "#dfd2a4",
     "textColor": "#000",
-    "errorColor": "#4a3912",
-    "infoColor": "#ab465a",
-    "successColor": "#524c3c"
+    "errorColor": "#725531",
+    "infoColor": "#000",
+    "successColor": "#4e432d"
   },
   "images": [
     {
-      "name": "Pfau",
-      "shortName": "Pfau",
-      "source": "detail-theaterberg-pfau",
-      "description": "",
-      "link": "",
-      "wiki": ""
-    },
-    {
-      "name": "Camel",
+      "name": "Das Zuckerbankett",
       "shortName": "Camel",
       "source": "detail-zuckerbankett-camel",
-      "description": "",
-      "link": "",
-      "wiki": ""
+      "description": "Der Kupferstich zeigt das sogenannte „Zuckerbankett“. Dieses wurde zum Abschluss des Hochzeitstages in der Hofstube des Düsseldorfer Schlosses präsentiert. So schreibt es Graminäus in seinem Werk. Zu sehen sind nicht nur, wie es der Name vielleicht vermuten lässt, Objekte aus Zucker, sondern auch Tafelaufsätze, Pokale und Scherztrinkgefäße. Auf der prächtigen und detailreichen Schautafel gibt es einiges zu entdecken.",
+      "link": "Was ist zu sehen?",
+      "footer": "Der Festtisch ist mit unzähligen Figuren und Objekten geschmückt. Ins Auge fällt zugleich die dargestellte Burg und die ungewöhnlichen Tierwesen. Auf dem Tisch befinden sich zum Beispiel ein Einhorn, ein Kriegselefant mit Soldaten, aber auch Meerestiere und Orangenbäumchen. Dabei waren viele der Tiere Scherztrinkgefäße, was gemeinsam mit den am Rand platzierten Tellern und Servietten darauf hinweist, dass an dieser Tafel auch gespeist werden sollte. Manche Figuren, wie die Wappen tragenden Löwen, bestanden sogar komplett aus Zucker. Vor dem Bankett sieht man zwei Paare, die Gäste der Festlichkeiten waren. Durch ihre Kleidung, die spanische Tracht, wissen wir, dass sie zum Adel gehörten."
     },
     {
-      "name": "Elefant",
+      "name": "Theater in Pempelfort mit Aufführung der Orpheus-Sage",
+      "shortName": "Einhorn",
+      "source": "detail-theaterberg-einhorn",
+      "description": "Auf den Kupferstich ist die Kulisse für ein Sinnspiel dargestellt. Das aufgeführte Stück „Orpheus und Amphion“ gilt als erstes opernartiges theatralisches Schauspiel mit Gesang und Musik in Deutschland. Man kann sagen, hier im Jahr 1585 beginnt die Theatergeschichte in Düsseldorf.",
+      "link": "Was ist zu sehen?",
+      "footer": "Wir sehen auf dieser Grafik einen großen Berg mit zwei Gipfeln, der die Kulisse des Theaterstücks „Orpehus und Amphion“ war. Auf dem vorderen Gipfel sitzt Amphion, auf dem anderen Orpheus. Beides sind Figuren aus der griechischen Sagenwelt. Sie verbindet die Musik, denn beide spielen in der Szene ein Zupfinstrument. Amphion hat die Zyther in der Hand, Orpheus die Lyra. Letzterer ist umringt von wilden Tieren, die er mit seiner Musik friedlich stimmte. Mauern und Stadt von Theben bildeten den Bühnenrahmen, in dessen Hintergrund sich der mit Tieren, Pflanzen und Bäumen belebte Berg erhob. Wenn die Proportionen auf dem Kupferstich einigermaßen korrekt wiedergegeben wurden, hat die Bühne eine beachtliche Größe besessen; zumindest Orpheus und Amphion sind durch lebende, musizierende Personen dargestellt worden. Vermutlich war die Bühne auf riesigen Wagen angebracht."
+    },
+    {
+      "name": "Das Zuckerbankett",
       "shortName": "Elefant",
       "source": "detail-zuckerbankett-elefant",
-      "description": "",
-      "link": "",
-      "wiki": ""
+      "description": "Der Kupferstich zeigt das sogenannte „Zuckerbankett“. Dieses wurde zum Abschluss des Hochzeitstages in der Hofstube des Düsseldorfer Schlosses präsentiert. So schreibt es Graminäus in seinem Werk. Zu sehen sind nicht nur, wie es der Name vielleicht vermuten lässt, Objekte aus Zucker, sondern auch Tafelaufsätze, Pokale und Scherztrinkgefäße. Auf der prächtigen und detailreichen Schautafel gibt es einiges zu entdecken.",
+      "link": "Was ist zu sehen?",
+      "footer": "Der Festtisch ist mit unzähligen Figuren und Objekten geschmückt. Ins Auge fällt zugleich die dargestellte Burg und die ungewöhnlichen Tierwesen. Auf dem Tisch befinden sich zum Beispiel ein Einhorn, ein Kriegselefant mit Soldaten, aber auch Meerestiere und Orangenbäumchen. Dabei waren viele der Tiere Scherztrinkgefäße, was gemeinsam mit den am Rand platzierten Tellern und Servietten darauf hinweist, dass an dieser Tafel auch gespeist werden sollte. Manche Figuren, wie die Wappen tragenden Löwen, bestanden sogar komplett aus Zucker. Vor dem Bankett sieht man zwei Paare, die Gäste der Festlichkeiten waren. Durch ihre Kleidung, die spanische Tracht, wissen wir, dass sie zum Adel gehörten."
     },
     {
-      "name": "Hirsch",
+      "name": "Das Zuckerbankett",
       "shortName": "Hirsch",
       "source": "detail-zuckerbankett-hirsch",
-      "description": "",
-      "link": "",
-      "wiki": ""
+      "description": "Der Kupferstich zeigt das sogenannte „Zuckerbankett“. Dieses wurde zum Abschluss des Hochzeitstages in der Hofstube des Düsseldorfer Schlosses präsentiert. So schreibt es Graminäus in seinem Werk. Zu sehen sind nicht nur, wie es der Name vielleicht vermuten lässt, Objekte aus Zucker, sondern auch Tafelaufsätze, Pokale und Scherztrinkgefäße. Auf der prächtigen und detailreichen Schautafel gibt es einiges zu entdecken.",
+      "link": "Was ist zu sehen?",
+      "footer": "Der Festtisch ist mit unzähligen Figuren und Objekten geschmückt. Ins Auge fällt zugleich die dargestellte Burg und die ungewöhnlichen Tierwesen. Auf dem Tisch befinden sich zum Beispiel ein Einhorn, ein Kriegselefant mit Soldaten, aber auch Meerestiere und Orangenbäumchen. Dabei waren viele der Tiere Scherztrinkgefäße, was gemeinsam mit den am Rand platzierten Tellern und Servietten darauf hinweist, dass an dieser Tafel auch gespeist werden sollte. Manche Figuren, wie die Wappen tragenden Löwen, bestanden sogar komplett aus Zucker. Vor dem Bankett sieht man zwei Paare, die Gäste der Festlichkeiten waren. Durch ihre Kleidung, die spanische Tracht, wissen wir, dass sie zum Adel gehörten."
     },
     {
-      "name": "Hund",
-      "shortName": "Hund",
-      "source": "detail-zuckerbankett-hund-1",
-      "description": "",
-      "link": "",
-      "wiki": ""
+      "name": "„Kampf des Drachens mit dem Walfisch“ am 22. Juni 1585",
+      "shortName": "Wal und Drache",
+      "source": "detail-wal-und-drache",
+      "description": "Der 22. Juni, also der vorletzte Tag der Hochzeitfestlichkeiten, wurde mit einem Feuerwerk auf dem Rhein abgeschlossen, welches auf dem Kupferstich „Kampf des Drachens mit dem Walfisch“ dargestellt ist. Es war damals üblich, dass Großereignisse wie Hochzeiten oder Taufen, an Fürstenhöfen auch mit Feuerwerken gefeiert wurden.",
+      "link": "Was ist zu sehen?",
+      "footer": "Der Kupferstich vermittelt den Betrachter*innen den Eindruck, dass damals wirklich ein Drache gegen einen riesigen Walfisch auf dem Rhein kämpfte. Stattdessen sieht man hier aber ein Feuerwerk. Im 16. Jahrhundert gab es natürlich noch keine Feuerwerksraketen, sondern man nutze zum Beispiel Groß- und Kleinpulver. Die beiden großen Figuren waren vermutlich auf Flößen angebracht, die kaum sichtbar waren. Auf beiden Seiten kämpften Soldaten, auch in Kostümen gekleidet, gegeneinander. Sie konnten aber nicht fliegen, wie das Bild die Situation widerspiegelt. Die Darstellungsweise des Kupferstechers macht die Szene aber sicherlich aufregender. Im Hintergrund ist die Altstadt von Düsseldorf zu erkennen."
     },
     {
-      "name": "Kranich",
+      "name": "Das Zuckerbankett",
       "shortName": "Kranich",
       "source": "detail-zuckerbankett-kranich",
-      "description": "",
-      "link": "",
-      "wiki": ""
+      "description": "Der Kupferstich zeigt das sogenannte „Zuckerbankett“. Dieses wurde zum Abschluss des Hochzeitstages in der Hofstube des Düsseldorfer Schlosses präsentiert. So schreibt es Graminäus in seinem Werk. Zu sehen sind nicht nur, wie es der Name vielleicht vermuten lässt, Objekte aus Zucker, sondern auch Tafelaufsätze, Pokale und Scherztrinkgefäße. Auf der prächtigen und detailreichen Schautafel gibt es einiges zu entdecken.",
+      "link": "Was ist zu sehen?",
+      "footer": "Der Festtisch ist mit unzähligen Figuren und Objekten geschmückt. Ins Auge fällt zugleich die dargestellte Burg und die ungewöhnlichen Tierwesen. Auf dem Tisch befinden sich zum Beispiel ein Einhorn, ein Kriegselefant mit Soldaten, aber auch Meerestiere und Orangenbäumchen. Dabei waren viele der Tiere Scherztrinkgefäße, was gemeinsam mit den am Rand platzierten Tellern und Servietten darauf hinweist, dass an dieser Tafel auch gespeist werden sollte. Manche Figuren, wie die Wappen tragenden Löwen, bestanden sogar komplett aus Zucker. Vor dem Bankett sieht man zwei Paare, die Gäste der Festlichkeiten waren. Durch ihre Kleidung, die spanische Tracht, wissen wir, dass sie zum Adel gehörten."
     },
     {
-      "name": "Orangenbaum II",
-      "shortName": "Orangenbaum II",
-      "source": "detail-zuckerbankett-orangenbaum-2",
-      "description": "",
-      "link": "",
-      "wiki": ""
+      "name": "Theater in Pempelfort mit Aufführung der Orpheus-Sage",
+      "shortName": "Löwe",
+      "source": "detail-theaterberg-loewe",
+      "description": "Auf den Kupferstich ist die Kulisse für ein Sinnspiel dargestellt. Das aufgeführte Stück „Orpheus und Amphion“ gilt als erstes opernartiges theatralisches Schauspiel mit Gesang und Musik in Deutschland. Man kann sagen, hier im Jahr 1585 beginnt die Theatergeschichte in Düsseldorf.",
+      "link": "Was ist zu sehen?",
+      "footer": "Wir sehen auf dieser Grafik einen großen Berg mit zwei Gipfeln, der die Kulisse des Theaterstücks „Orpehus und Amphion“ war. Auf dem vorderen Gipfel sitzt Amphion, auf dem anderen Orpheus. Beides sind Figuren aus der griechischen Sagenwelt. Sie verbindet die Musik, denn beide spielen in der Szene ein Zupfinstrument. Amphion hat die Zyther in der Hand, Orpheus die Lyra. Letzterer ist umringt von wilden Tieren, die er mit seiner Musik friedlich stimmte. Mauern und Stadt von Theben bildeten den Bühnenrahmen, in dessen Hintergrund sich der mit Tieren, Pflanzen und Bäumen belebte Berg erhob. Wenn die Proportionen auf dem Kupferstich einigermaßen korrekt wiedergegeben wurden, hat die Bühne eine beachtliche Größe besessen; zumindest Orpheus und Amphion sind durch lebende, musizierende Personen dargestellt worden. Vermutlich war die Bühne auf riesigen Wagen angebracht."
     },
     {
-      "name": "Orangenbaum",
+      "name": "Das Zuckerbankett",
       "shortName": "Orangenbaum",
       "source": "detail-zuckerbankett-orangenbaum",
-      "description": "",
-      "link": "",
-      "wiki": ""
+      "description": "Der Kupferstich zeigt das sogenannte „Zuckerbankett“. Dieses wurde zum Abschluss des Hochzeitstages in der Hofstube des Düsseldorfer Schlosses präsentiert. So schreibt es Graminäus in seinem Werk. Zu sehen sind nicht nur, wie es der Name vielleicht vermuten lässt, Objekte aus Zucker, sondern auch Tafelaufsätze, Pokale und Scherztrinkgefäße. Auf der prächtigen und detailreichen Schautafel gibt es einiges zu entdecken.",
+      "link": "Was ist zu sehen?",
+      "footer": "Der Festtisch ist mit unzähligen Figuren und Objekten geschmückt. Ins Auge fällt zugleich die dargestellte Burg und die ungewöhnlichen Tierwesen. Auf dem Tisch befinden sich zum Beispiel ein Einhorn, ein Kriegselefant mit Soldaten, aber auch Meerestiere und Orangenbäumchen. Dabei waren viele der Tiere Scherztrinkgefäße, was gemeinsam mit den am Rand platzierten Tellern und Servietten darauf hinweist, dass an dieser Tafel auch gespeist werden sollte. Manche Figuren, wie die Wappen tragenden Löwen, bestanden sogar komplett aus Zucker. Vor dem Bankett sieht man zwei Paare, die Gäste der Festlichkeiten waren. Durch ihre Kleidung, die spanische Tracht, wissen wir, dass sie zum Adel gehörten."
     },
     {
-      "name": "Schildkröte",
-      "shortName": "Schildkröte",
-      "source": "detail-zuckerbankett-schildkroete",
-      "description": "",
-      "link": "",
-      "wiki": ""
-    },
-    {
-      "name": "Ziegenbock",
+      "name": "Das Zuckerbankett",
       "shortName": "Ziegenbock",
       "source": "detail-zuckerbankett-ziegenbock",
-      "description": "",
-      "link": "",
-      "wiki": ""
+      "description": "Der Kupferstich zeigt das sogenannte „Zuckerbankett“. Dieses wurde zum Abschluss des Hochzeitstages in der Hofstube des Düsseldorfer Schlosses präsentiert. So schreibt es Graminäus in seinem Werk. Zu sehen sind nicht nur, wie es der Name vielleicht vermuten lässt, Objekte aus Zucker, sondern auch Tafelaufsätze, Pokale und Scherztrinkgefäße. Auf der prächtigen und detailreichen Schautafel gibt es einiges zu entdecken.",
+      "link": "Was ist zu sehen?",
+      "footer": "Der Festtisch ist mit unzähligen Figuren und Objekten geschmückt. Ins Auge fällt zugleich die dargestellte Burg und die ungewöhnlichen Tierwesen. Auf dem Tisch befinden sich zum Beispiel ein Einhorn, ein Kriegselefant mit Soldaten, aber auch Meerestiere und Orangenbäumchen. Dabei waren viele der Tiere Scherztrinkgefäße, was gemeinsam mit den am Rand platzierten Tellern und Servietten darauf hinweist, dass an dieser Tafel auch gespeist werden sollte. Manche Figuren, wie die Wappen tragenden Löwen, bestanden sogar komplett aus Zucker. Vor dem Bankett sieht man zwei Paare, die Gäste der Festlichkeiten waren. Durch ihre Kleidung, die spanische Tracht, wissen wir, dass sie zum Adel gehörten."
+    },
+    {
+      "name": "Theater in Pempelfort mit Aufführung der Orpheus-Sage",
+      "shortName": "Hund",
+      "source": "detail-theaterberg-hund",
+      "description": "Auf den Kupferstich ist die Kulisse für ein Sinnspiel dargestellt. Das aufgeführte Stück „Orpheus und Amphion“ gilt als erstes opernartiges theatralisches Schauspiel mit Gesang und Musik in Deutschland. Man kann sagen, hier im Jahr 1585 beginnt die Theatergeschichte in Düsseldorf.",
+      "link": "Was ist zu sehen?",
+      "footer": "Wir sehen auf dieser Grafik einen großen Berg mit zwei Gipfeln, der die Kulisse des Theaterstücks „Orpehus und Amphion“ war. Auf dem vorderen Gipfel sitzt Amphion, auf dem anderen Orpheus. Beides sind Figuren aus der griechischen Sagenwelt. Sie verbindet die Musik, denn beide spielen in der Szene ein Zupfinstrument. Amphion hat die Zyther in der Hand, Orpheus die Lyra. Letzterer ist umringt von wilden Tieren, die er mit seiner Musik friedlich stimmte. Mauern und Stadt von Theben bildeten den Bühnenrahmen, in dessen Hintergrund sich der mit Tieren, Pflanzen und Bäumen belebte Berg erhob. Wenn die Proportionen auf dem Kupferstich einigermaßen korrekt wiedergegeben wurden, hat die Bühne eine beachtliche Größe besessen; zumindest Orpheus und Amphion sind durch lebende, musizierende Personen dargestellt worden. Vermutlich war die Bühne auf riesigen Wagen angebracht."
+    },
+    {
+      "name": "Das Zuckerbankett",
+      "shortName": "Vögel",
+      "source": "detail-zuckerbankett-voegel",
+      "description": "Der Kupferstich zeigt das sogenannte „Zuckerbankett“. Dieses wurde zum Abschluss des Hochzeitstages in der Hofstube des Düsseldorfer Schlosses präsentiert. So schreibt es Graminäus in seinem Werk. Zu sehen sind nicht nur, wie es der Name vielleicht vermuten lässt, Objekte aus Zucker, sondern auch Tafelaufsätze, Pokale und Scherztrinkgefäße. Auf der prächtigen und detailreichen Schautafel gibt es einiges zu entdecken.",
+      "link": "Was ist zu sehen?",
+      "footer": "Der Festtisch ist mit unzähligen Figuren und Objekten geschmückt. Ins Auge fällt zugleich die dargestellte Burg und die ungewöhnlichen Tierwesen. Auf dem Tisch befinden sich zum Beispiel ein Einhorn, ein Kriegselefant mit Soldaten, aber auch Meerestiere und Orangenbäumchen. Dabei waren viele der Tiere Scherztrinkgefäße, was gemeinsam mit den am Rand platzierten Tellern und Servietten darauf hinweist, dass an dieser Tafel auch gespeist werden sollte. Manche Figuren, wie die Wappen tragenden Löwen, bestanden sogar komplett aus Zucker. Vor dem Bankett sieht man zwei Paare, die Gäste der Festlichkeiten waren. Durch ihre Kleidung, die spanische Tracht, wissen wir, dass sie zum Adel gehörten."
     }
   ]
 };
