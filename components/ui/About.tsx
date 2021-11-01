7c991b8fcde60b2cd02c74be3cd723c8f3cea4d1
Gerd Müller <g.mueller@ewerk.com>
2021-11-01 20:44:30 +0100

    Adjust about/start text

--- components/ui/About.tsx
+++ components/ui/About.tsx
@@ -6,60 +6,72 @@ export function About({ onClose, open }: { onClose: () => void; open: boolean })
   };
   return (
     <Dialog onClose={handleClose} aria-labelledby="about-dialog" open={open} maxWidth={'xl'}>
-      <DialogTitle id="about-dialog">Über Plantala</DialogTitle>
+      <DialogTitle id="about-dialog">Über Julala</DialogTitle>
       <DialogContent>
         <DialogContentText>
-          Wir, Anne Mühlich und Gerd Müller, haben 2021 die Webanwendung Plantala im Rahmen des Kulturhackathons Coding
-          da Vinci und eines anschließenden Stipendiums entwickelt. Mit Plantala kannst du aus ästhetischen
-          Pflanzenteilen dein eigenes Mandala erstellen, speichern, ausdrucken und im Anschluss ausmalen. Ganz nebenbei
-          lernst du auch noch etwas über spezielle Eigenheiten der einzelnen Pflanzen.
+          Das Zuckerbankett, der Kampf eines Wals mit einem Drachen, spektakuläre Feuerwerke und ein ganzer sich
+          bewegender Berg als Theaterkulisse – auf den alten Kupferstichen ist so viel zu entdecken. Nimm die kleinen
+          Details und setze sie zu etwas Neuem zusammen. Mit Julala kannst du dein eigenes Mandala erstellen, speichern,
+          ausdrucken und im Anschluss ausmalen.
         </DialogContentText>
         <DialogContentText>
-          Alle Bilder und erstellten Plantalas sind frei lizenziert und können vielfältig genutzt werden, z.B. für den
-          Unterricht, für umweltpädagogische Angebote oder einfach für private Deko. Verweise gern auf uns und nutze den
-          Hashtag <i> #plantala</i>
+          Nebenbei erfährst du etwas über eine wichtige politische Hochzeit im 16. Jahrhundert in Düsseldorf und
+          darüber, wie man in der Renaissance Feste feierte.
+        </DialogContentText>
+        <DialogContentText>
+          Alle Schwarz-Weiß-Bilder und die erstellten Julalas sind frei lizenziert und können vielfältig genutzt werden,
+          z. B. für den Unterricht, für museumspädagogische Angebote oder einfach für private Deko. Verweise gern auf
+          uns (Liane Hellmund, Anne und Gerd) und nutze den Hashtag <i>#julala</i>
         </DialogContentText>
       </DialogContent>
-      <DialogTitle>Hintergründe</DialogTitle>
+      <DialogTitle>Der Hintergrund:</DialogTitle>
       <DialogContent>
         <DialogContentText>
-          Die Lehrtafeln entstanden gegen Ende des 19. bis Anfang des 20. Jahrhunderts. Oft ist dabei nicht genau
-          bekannt, wer die Tafeln gemalt hat. An den von uns genutzten Tafeln waren nach heutigem Wissensstand Carl
-          Bollmann, Hermann Zippel, Albert Peter, Walter Müller sowie bisher unbekannte Zeichner beteiligt. Einige der
-          Lehrtafeln erschienen im Verlag von Friedrich Vieweg und Sohn.
+          Das Stadtmuseum Düsseldorf hat im Rahmen des Kultur-Hackathons Coding da Vinci Nieder.Rhein.Land 2021 die
+          Objektdatensätze zur „Fürstlich Jülich´sche Hochzeit“ bereitgestellt. (Lizenz CC BY 4.0 Stadtmuseum
+          Düsseldorf)
         </DialogContentText>
         <DialogContentText>
-          Der Quellcode von Plantala läuft unter einer MIT Lizenz. Die von der Universität Göttingen bereitgestellten
-          Digitalisate von Botanischen Lehrtafeln wurden teilweise unter einer CC BY-SA 4.0 Lizenz veröffentlicht. Die
-          vorher in publizierten Werken erschienenen Lehrtafeln laufen unter einer Publik Domain Mark 1.0 und sind damit
-          gemeinfrei. Dies gilt auch für die bunten Pflanzenausschnitte.{' '}
-          <Link href="https://sammlungen.uni-goettingen.de/sammlung/slg_1053/" target="_blank" rel="noreferrer">
-            Hier
-          </Link>{' '}
-          findest du alle Lehrtafeln aus der Sammlung der Uni Göttingen. Alle eigens für das Projekt erstellten
-          schwarz-weißen Pflanzenvektoren laufen unter einer CC BY-SA 4.0 Lizenz.
+          Die Kupferstiche stammen aus der Werkstatt von Franz Hogenberg und zeigen die Ereignisse rund um die Hochzeit
+          von Herzog Johann Wilhelm von Jülich-Kleve-Berg mit der Markgräfin Jakobe von Baden im Juni 1585.
+        </DialogContentText>
+        <DialogContentText>
+          Es war DAS politische Großereignis im 16. Jahrhundert in Düsseldorf. Acht Tage lang wurde die Hochzeit mit den
+          unterschiedlichsten Veranstaltungen gefeiert. Über 1.000 adelige Gäste waren geladen.
+        </DialogContentText>
+        <DialogContentText>
+          Die Grafiken überliefern die Hochzeitsfeier für die Nachwelt. Zudem enthalten sie die ältesten Stadt- und
+          Schlossansichten Düsseldorfs.
         </DialogContentText>
       </DialogContent>
-      <DialogTitle>Plantala weiterdenken</DialogTitle>
+      <DialogTitle>Die Idee:</DialogTitle>
       <DialogContent>
         <DialogContentText>
-          Plantala ist als Open-Source-Anwendung für eine Medienstation konzipiert und soll Kulturinstitutionen die
-          Möglichkeit bieten, ihre eigene Medienstation zu entwickeln. Die Medienstation as a Service (MaaS) bietet dir
-          eine einfache Möglichkeit, die technischen Vorzüge Plantalas mit deinen Bildern zu verbinden. Willst du mehr
-          erfahren? Schau in die{' '}
-          <Link
-            href="https://github.com/digitalwarenkombinat/plantala/blob/main/medienstationAsAService/README.md"
-            target="_blank"
-            rel="noreferrer"
-          >
-            ausführliche Beschreibung
-          </Link>{' '}
-          von MaaS. Hast du Interesse, MaaS einzusetzen oder weiterzuentwickeln? Oder hast du bei Plantala inhaltliche
-          Fehler gefunden, die du gern korrigiert sehen möchtest? Dann kontaktiere uns unter folgender E-Mail-Adresse:{' '}
-          <Link href="mailto:info@digitalwarenkombinat.de">info@digitalwarenkombinat.de</Link>
+          Die Kupferstiche zeigen eine Fülle von Details und fantastischen Szenen. Ich hatte sofort die Idee, daraus ein
+          Mal- oder Wimmelbuch für Kinder von 10 bis 100 zu machen. Damit möchte ich einen niederschwelligen Zugang zu
+          den kulturhistorisch sehr bedeutsamen Kupferstichen aus dem Jahr 1585 schaffen.
         </DialogContentText>
         <DialogContentText>
-          Folge uns gern auf{' '}
+          Aber das Museum wünschte sich auch einen Weg in die digitale Welt. Mit „Julala“ gibt es jetzt eine
+          Webanwendung, mit der man eigene Mandalas kreieren kann. So entsteht immer neues Material für malfreudige
+          Menschen, für Lehrer*innen, für Museumspädagog*innen …
+        </DialogContentText>
+        <DialogContentText>
+          Ich hoffe sehr, dass das Stadtmuseum Düsseldorf Spaß an dem Entstandenen findet und es weiter nutzt. Denkbar
+          ist auch eine Nutzung durch das Theatermuseum, weil die Hochzeit gleichzeitig als Beginn der Theatergeschichte
+          in Düsseldorf angesehen wird.
+        </DialogContentText>
+        <DialogContentText>
+          Alle für das Projekt erstellten Vektorgrafiken wurden mit einer CC BY-SA 4.0 Lizenz versehen.
+        </DialogContentText>
+      </DialogContent>
+      <DialogTitle>Die Kooperation:</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Danke an Anne und Gerd! Sie haben 2021 die Webanwendung Plantala im Rahmen des Kulturhackathons Coding da
+          Vinci Niedersachsen und eines anschließenden Stipendiums entwickelt. Mit Julala gibt es nun die erste
+          gewünschte Nachnutzung ihrer Applikation. Gerd und Anne übernahmen die Einbindung der Schwarz-Weiß-Grafiken in
+          ihre Software. (Folge den beiden auf{' '}
           <Link href="https://twitter.com/AnkaTrabantka" target="_blank" rel="noreferrer">
             Twitter
           </Link>{' '}
@@ -67,7 +79,13 @@ export function About({ onClose, open }: { onClose: () => void; open: boolean })
           <Link href="https://www.instagram.com/ankatrabantka" target="_blank" rel="noreferrer">
             Instagram
           </Link>{' '}
-          unter <i> @AnkaTrabantka </i>
+          unter <i> @AnkaTrabantka </i>)
+        </DialogContentText>
+        <DialogContentText>Der Quellcode von Plantala/Julala läuft unter einer MIT Lizenz.</DialogContentText>
+        <DialogContentText>
+          Du hast eine Idee für eine weitere Nachnutzung? Du wünscht dir auch eine App, die dir Mandalas mit deinen
+          Motiven oder zu einem bestimmten Thema generiert? Dann kontaktiere die beiden unter folgender E-Mail-Adresse:
+          <Link href="mailto:info@digitalwarenkombinat.de">info@digitalwarenkombinat.de</Link>
         </DialogContentText>
       </DialogContent>
     </Dialog>
--- components/ui/StartBanner.tsx
+++ components/ui/StartBanner.tsx
@@ -8,11 +8,19 @@ const useStyles = makeStyles((theme: Theme) => ({
     display: 'flex',
     flexDirection: 'column',
   },
+  subTitle: {
+    padding: theme.spacing(2, 0),
+    fontWeight: 'bold',
+  },
 }));
 
 const main = {
   title: 'Wissen! Malen! Entspannen!',
-  description: 'Erstelle individuelle Mandalas mit Details aus historischen Kupferstichen.',
+  subTitle: 'Tauche ein in die Welt der Renaissance.',
+  description:
+    'Es geht um die Hochzeit von Herzog Johann Wilhelm von Jülich-Kleve-Berg mit der Markgräfin Jakobe von Baden im Juni 1585.',
+  subDescription:
+    'Es geht um Feuerwerke, einem Kampf zwischen einem Drachen und einem Wal auf dem Rhein, um Theaterspiel und ein Zuckerbankett…',
 };
 
 export default function Header() {
@@ -20,11 +28,17 @@ export default function Header() {
 
   return (
     <Grid item xs={12} md={6} className={classes.banner}>
-      <Typography variant="h3" component="h2" gutterBottom>
+      <Typography variant="h4" component="h2" gutterBottom>
         {main.title}
       </Typography>
-      <Typography variant="h4" component="h3" paragraph>
+      <Typography variant="h5" component="h3" paragraph className={classes.subTitle}>
+        {main.subTitle}
+      </Typography>
+      <Typography variant="h5" component="h4" paragraph>
         {main.description}
+      </Typography>
+      <Typography variant="h5" component="h4" paragraph>
+        {main.subDescription}
       </Typography>
     </Grid>
   );
